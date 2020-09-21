7f1f0f6d92f5ee2e8ad904b9c35937f2c1625403
Huag <teeradet.huag@gmail.com>
2020-09-21 18:43:56 +0700

    🐱add zero uuid

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.12
 
 require (
 	4d63.com/tz v1.2.0
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+	github.com/gofrs/uuid v3.3.0+incompatible
 	github.com/jmoiron/sqlx v1.2.0
 )
---
+++ zero_uuid.go
@@ -0,0 +1,185 @@
+package models
+
+import (
+	"database/sql/driver"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+
+	"github.com/globalsign/mgo/bson"
+	"github.com/gofrs/uuid"
+)
+
+var (
+	byteGroups = []int{8, 4, 4, 4, 12}
+)
+
+type ZeroUUID uuid.UUID
+
+func NewZeroUUIDFromstring(uidStr string) (ZeroUUID, error) {
+	uid, err := uuid.FromString(uidStr)
+	if err != nil {
+		return ZeroUUID(uuid.Nil), nil
+	}
+	return ZeroUUID(uid), nil
+}
+
+func NewZeroUUIDFromUUID(uid *uuid.UUID) (ZeroUUID, error) {
+	if uid == nil {
+		return ZeroUUID(uuid.Nil), nil
+	}
+
+	return ZeroUUID(*uid), nil
+}
+
+func NewV4() ZeroUUID {
+	uid, _ := uuid.NewV4()
+	return ZeroUUID(uid)
+}
+
+func (zu ZeroUUID) IsZero() bool {
+	if zu == ZeroUUID((uuid.UUID{})) {
+		return true
+	}
+
+	return false
+}
+
+func (zu ZeroUUID) ToUUID() *uuid.UUID {
+	if zu == ZeroUUID(uuid.Nil) {
+		return nil
+	}
+
+	uid := uuid.UUID(zu)
+	return &uid
+}
+
+func (zu ZeroUUID) ToBsonBinary() *bson.Binary {
+	if zu == ZeroUUID(uuid.Nil) {
+		return nil
+	}
+
+	uid := uuid.UUID(zu)
+	return &bson.Binary{
+		Kind: bson.BinaryUUID,
+		Data: uid.Bytes(),
+	}
+}
+
+func (zu ZeroUUID) NullUUID() NullUUID {
+	var nullUID = NullUUID{}
+	if zu == ZeroUUID((uuid.UUID{})) {
+		nullUID.UUID = ZeroUUID((uuid.UUID{}))
+		nullUID.Valid = false
+		return nullUID
+	}
+
+	nullUID.UUID = zu
+	nullUID.Valid = true
+	return nullUID
+}
+
+func (zu ZeroUUID) Interface() interface{} {
+	if zu == ZeroUUID((uuid.UUID{})) {
+		return nil
+	}
+
+	return zu
+}
+
+func (zu ZeroUUID) MarshalJSON() ([]byte, error) {
+	if zu == ZeroUUID((uuid.UUID{})) {
+		return json.Marshal("")
+	}
+	return json.Marshal(uuid.UUID(zu).String())
+}
+
+func (zu ZeroUUID) String() string {
+	if zu == ZeroUUID((uuid.UUID{})) {
+		return ""
+	}
+	return uuid.UUID(zu).String()
+}
+
+func (zu *ZeroUUID) Scan(src interface{}) error {
+	switch src := src.(type) {
+	case ZeroUUID: // support gorm convert from UUID to NullUUID
+		*zu = src
+		return nil
+
+	case []byte:
+		if len(src) == uuid.Size {
+			return zu.UnmarshalBinary(src)
+		}
+		return zu.UnmarshalText(src)
+
+	case string:
+		return zu.UnmarshalText([]byte(src))
+	}
+
+	return fmt.Errorf("uuid: cannot convert %T to UUID", src)
+}
+
+func (zu ZeroUUID) Value() (driver.Value, error) {
+	if zu == ZeroUUID(uuid.Nil) {
+		return nil, nil
+	}
+	return zu.String(), nil
+}
+
+func (zu *ZeroUUID) UnmarshalBinary(data []byte) error {
+	if len(data) != uuid.Size {
+		return fmt.Errorf("uuid: UUID must be exactly 16 bytes long, got %d bytes", len(data))
+	}
+	copy(zu[:], data)
+
+	return nil
+}
+
+func (zu ZeroUUID) MarshalBinary() ([]byte, error) {
+	return uuid.UUID(zu).Bytes(), nil
+}
+
+func (zu *ZeroUUID) UnmarshalText(text []byte) error {
+	switch len(text) {
+	case 32:
+		return zu.decodeHashLike(text)
+	case 36:
+		return zu.decodeCanonical(text)
+	default:
+		return fmt.Errorf("uuid: incorrect UUID length %d in string %q", len(text), text)
+	}
+}
+
+// decodeHashLike decodes UUID strings that are using the following format:
+//  "6ba7b8109dad11d180b400c04fd430c8".
+func (u *ZeroUUID) decodeHashLike(t []byte) error {
+	src := t[:]
+	dst := u[:]
+
+	_, err := hex.Decode(dst, src)
+	return err
+}
+
+func (u *ZeroUUID) decodeCanonical(t []byte) error {
+	if t[8] != '-' || t[13] != '-' || t[18] != '-' || t[23] != '-' {
+		return fmt.Errorf("uuid: incorrect UUID format in string %q", t)
+	}
+
+	src := t
+	dst := u[:]
+
+	for i, byteGroup := range byteGroups {
+		if i > 0 {
+			src = src[1:] // skip dash
+		}
+		_, err := hex.Decode(dst[:byteGroup/2], src[:byteGroup])
+		if err != nil {
+			return err
+		}
+		src = src[byteGroup:]
+		dst = dst[byteGroup/2:]
+	}
+
+	return nil
+}
